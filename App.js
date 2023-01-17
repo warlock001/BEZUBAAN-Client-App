a684f7619103b6acf96fe8bb0c41650e73ee6092
Zabloon <zabloona@gmail.com>
2023-01-17 06:12:57 +0500

    added

--- App.js
+++ App.js
@@ -22,9 +22,10 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
+        <Stack.Screen name="RescueCenter" component={RescueCenter} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="RescueCenter" component={RescueCenter} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="ReportAnimal" component={MyAccount} />
 
--- screens/rescueCenter.js
+++ screens/rescueCenter.js
@@ -16,13 +16,15 @@ import {
 import { connectToSocket, socket } from '../sockets/socketConfig';
 import { useFocusEffect } from '@react-navigation/native';
 import React, { useState, useRef, useEffect } from 'react';
-const REACT_APP_BASE_URL = "http://192.168.100.76:3001";
+const REACT_APP_BASE_URL = "http://192.168.0.107:3001";
 
 export default function RescueCenter({ navigation }) {
     const { width: PAGE_WIDTH, height: PAGE_HEIGHT } = Dimensions.get('window');
+    const [modalVisible, setModalVisible] = useState(false);
+    const [rescueLocation, setRescueLocation] = useState(false);
 
     useFocusEffect(
-        React.useCallback(async () => {
+        React.useCallback(() => {
 
             connectSocket = async () => {
                 connectToSocket();
@@ -30,14 +32,94 @@ export default function RescueCenter({ navigation }) {
 
             connectSocket();
 
-            socket.on("hello", (arg) => {
+            socket.on("report", (arg) => {
                 console.log(arg); // world
+                setRescueLocation(arg)
+                setModalVisible(true)
             });
 
         })
     );
 
     return (
-        <View></View>
+        <View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalTitle}>Rescue Request!</Text>
+                        <Text style={styles.modalText}>{rescueLocation}</Text>
+                        <View style={{ display: 'flex', flexDirection: 'row', gap: '10' }}>
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => setModalVisible(!modalVisible)}>
+                                <Text style={styles.textStyle}>Accept</Text>
+                            </Pressable>
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => setModalVisible(!modalVisible)}>
+                                <Text style={styles.textStyle}>Cancel</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        margin: 10,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+
+    modalTitle: {
+        fontWeight: 'bold',
+        marginBottom: 15,
+    }
+});
