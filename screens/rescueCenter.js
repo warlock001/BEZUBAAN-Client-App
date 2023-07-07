1ef10edc46f202187de1240a90be9d3f0960fc16
Zabloon Albert <zabloona@gmail.com>
2023-07-07 06:07:44 +0500

    added

--- screens/rescueCenter.js
+++ screens/rescueCenter.js
@@ -15,22 +15,22 @@ import {
   Alert,
   KeyboardAvoidingView,
 } from 'react-native';
-import { connectToSocket, socket } from '../sockets/socketConfig';
-import { useFocusEffect } from '@react-navigation/native';
-import React, { useState, useRef, useEffect } from 'react';
+import {connectToSocket, socket} from '../sockets/socketConfig';
+import {useFocusEffect} from '@react-navigation/native';
+import React, {useState, useRef, useEffect} from 'react';
 import TextField from '../components/inputField';
-import { TextInput } from 'react-native-paper';
-import { Config } from '../config';
+import {TextInput} from 'react-native-paper';
+import {Config} from '../config';
 const REACT_APP_BASE_URL = Config.ip;
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import call from 'react-native-phone-call';
-import { set } from 'date-fns';
+import {set} from 'date-fns';
 import GetLocation from 'react-native-get-location';
 
 import getDirections from 'react-native-google-maps-directions';
-export default function RescueCenter({ navigation }) {
-  const { width: PAGE_WIDTH, height: PAGE_HEIGHT } = Dimensions.get('window');
+export default function RescueCenter({navigation}) {
+  const {width: PAGE_WIDTH, height: PAGE_HEIGHT} = Dimensions.get('window');
   const [modalVisible, setModalVisible] = useState(false);
   const [ETAmodalVisible, setETAModalVisible] = useState(false);
   const [rescueLocation, setRescueLocation] = useState(0);
@@ -39,45 +39,42 @@ export default function RescueCenter({ navigation }) {
   const [reportId, setReportId] = useState(0);
   const [ETA, setETA] = useState(0);
   const [Data, setData] = useState([]);
-  const [location, setLocation] = useState();
+  // const [location, setLocation] = useState();
   const [image, setImage] = useState();
   const [contentType, setContentType] = useState();
   const [shouldUpdate, setShouldUpdate] = useState(false);
-  const Item = ({ item, onPress, backgroundColor, textColor }) => {
+  var location;
+  const Item = ({item, onPress, backgroundColor, textColor}) => {
     const date = new Date(item.createdAt);
     // console.log(date);
 
-    const position = JSON.parse(item.location)
-
-
-    useFocusEffect(
-      React.useCallback(() => {
-        const getLocationNow = () => {
-          GetLocation.getCurrentPosition({
-            enableHighAccuracy: true,
-            timeout: 15000,
-          })
-            .then(location => {
-              console.log(location);
-              setLocation(location);
-            })
-            .catch(error => {
-              const { code, message } = error;
-              console.log(code, message);
-            });
-        };
-        getLocationNow();
-      }, [shouldUpdate])
-    )
-
-
+    const position = JSON.parse(item.location);
+    const getLocationNow = () => {
+      GetLocation.getCurrentPosition({
+        enableHighAccuracy: true,
+        timeout: 15000,
+      })
+        .then(res => {
+          console.log(res);
+          location = res;
+        })
+        .catch(error => {
+          const {code, message} = error;
+          console.log(code, message);
+        });
+    };
+    getLocationNow();
+    useFocusEffect(React.useCallback(() => {}, [shouldUpdate]));
 
     return (
       <TouchableOpacity
         onPress={onPress}
-        style={[styles.item, { backgroundColor: item.status == 'On Going' ? '#CF3339' : 'grey' }]}>
+        style={[
+          styles.item,
+          {backgroundColor: item.status == 'On Going' ? '#CF3339' : 'grey'},
+        ]}>
         <Text style={[styles.title]}>{date.toLocaleString()}</Text>
-        <Text style={[styles.title, { color: textColor }]}>{item.address}</Text>
+        <Text style={[styles.title, {color: textColor}]}>{item.address}</Text>
         <View
           style={{
             display: 'flex',
@@ -100,17 +97,22 @@ export default function RescueCenter({ navigation }) {
             }}>
             <Text>Get Direction</Text>
           </TouchableOpacity>
-          {item.status == "On Going" ? <TouchableOpacity onPress={() => {
-            markDone(item._id)
-          }}>
-            <Text>Mark Done</Text>
-          </TouchableOpacity> : ''}
+          {item.status == 'On Going' ? (
+            <TouchableOpacity
+              onPress={() => {
+                markDone(item._id);
+              }}>
+              <Text>Mark Done</Text>
+            </TouchableOpacity>
+          ) : (
+            ''
+          )}
         </View>
       </TouchableOpacity>
     );
   };
 
-  const renderItems = ({ item }) => {
+  const renderItems = ({item}) => {
     // console.log(item)
     const backgroundColor = item._id === selectedId ? '#6e3b6e' : '#f9c2ff';
     const color = item._id === selectedId ? 'white' : 'black';
@@ -143,11 +145,12 @@ export default function RescueCenter({ navigation }) {
 
         Alert.alert(
           'Failed',
-          `${er.response.data.message
-            ? er.response.data.message
-            : 'Something went wrong'
+          `${
+            er.response.data.message
+              ? er.response.data.message
+              : 'Something went wrong'
           }`,
-          [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+          [{text: 'OK', onPress: () => console.log('OK Pressed')}],
         );
       });
   }
@@ -171,21 +174,20 @@ export default function RescueCenter({ navigation }) {
 
       connectSocket();
 
-
       socket.on('report', arg => {
         console.log('fdfd');
         console.log(arg); // world
         setRescueLocation(arg.location);
         setReportId(arg.reportId);
         setModalVisible(true);
-        setImage(arg.image)
-        setContentType(arg.contentType)
+        setImage(arg.image);
+        setContentType(arg.contentType);
       });
     }, []),
   );
 
   async function markDone(reportId) {
-    console.log(reportId)
+    console.log(reportId);
     const id = await AsyncStorage.getItem('@id');
     const jwt = await AsyncStorage.getItem('@jwt');
     await axios({
@@ -224,11 +226,12 @@ export default function RescueCenter({ navigation }) {
 
         Alert.alert(
           'Failed',
-          `${er.response.data.message
-            ? er.response.data.message
-            : 'Something went wrong'
+          `${
+            er.response.data.message
+              ? er.response.data.message
+              : 'Something went wrong'
           }`,
-          [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+          [{text: 'OK', onPress: () => console.log('OK Pressed')}],
         );
       });
   }
@@ -256,11 +259,12 @@ export default function RescueCenter({ navigation }) {
 
         Alert.alert(
           'Failed',
-          `${er.response.data.message
-            ? er.response.data.message
-            : 'Something went wrong'
+          `${
+            er.response.data.message
+              ? er.response.data.message
+              : 'Something went wrong'
           }`,
-          [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+          [{text: 'OK', onPress: () => console.log('OK Pressed')}],
         );
       });
   }
@@ -278,16 +282,19 @@ export default function RescueCenter({ navigation }) {
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <Text style={styles.modalTitle}>Rescue Request!</Text>
-            <Image style={{
-              width: 250,
-              height: 250,
-              overflow: 'hidden',
-              // width: 70,
-              flexWrap: 'wrap',
-              marginBottom: 20
-            }} source={{ uri: `data:${contentType};base64,${image}` }} />
+            <Image
+              style={{
+                width: 250,
+                height: 250,
+                overflow: 'hidden',
+                // width: 70,
+                flexWrap: 'wrap',
+                marginBottom: 20,
+              }}
+              source={{uri: `data:${contentType};base64,${image}`}}
+            />
             <Text style={styles.modalText}>{rescueLocation}</Text>
-            <View style={{ display: 'flex', flexDirection: 'row', gap: '10' }}>
+            <View style={{display: 'flex', flexDirection: 'row', gap: '10'}}>
               <Pressable
                 style={[styles.button, styles.buttonClose]}
                 onPress={() => {
@@ -318,26 +325,26 @@ export default function RescueCenter({ navigation }) {
           <View style={styles.modalView}>
             <Text style={styles.modalTitle}>Provide ETA!</Text>
             <Text style={styles.modalText}>{rescueLocation}</Text>
-            <View style={{ display: 'flex', gap: '10' }}>
-              <View style={{ display: 'flex', flexDirection: 'row' }}>
+            <View style={{display: 'flex', gap: '10'}}>
+              <View style={{display: 'flex', flexDirection: 'row'}}>
                 <TextField
                   label="ETA"
                   onChangeText={text => setETA(text)}
-                  style={{ width: '80%' }}
+                  style={{width: '80%'}}
                   left={
                     <TextInput.Icon
                       resizeMode="contain"
-                      style={{ width: 25 }}
+                      style={{width: 25}}
                       name={() => (
                         <Image
                           resizeMode="contain"
-                          style={{ width: 25 }}
+                          style={{width: 25}}
                           source={require('../images/User1.png')}
                         />
                       )}
                     />
                   }
-                  right={<Text style={{ width: 25, color: 'black' }}>Mins</Text>}
+                  right={<Text style={{width: 25, color: 'black'}}>Mins</Text>}
                 />
                 <View
                   style={{
@@ -346,7 +353,7 @@ export default function RescueCenter({ navigation }) {
                     alignItems: 'center',
                     justifyContent: 'center',
                   }}>
-                  <Text style={{ color: 'black' }}>Mins</Text>
+                  <Text style={{color: 'black'}}>Mins</Text>
                 </View>
               </View>
               <Pressable
